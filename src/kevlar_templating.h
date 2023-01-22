8c012a6108c708fbab4642ddae0323cd563602c7
Aadv1k <aadv1k@outlook.com>
2023-01-22 20:21:39 +0530

    Majorly re-do the way templates and config is handled

--- src/kevlar_templating.h
+++ src/kevlar_templating.h
@@ -11,10 +11,8 @@
 #include "kevlar_handle_config.h"
 #include "kevlar_build.h"
 
-void kevlar_parse_template(FILE *in_file_buffer, FILE *out_file_buffer, KevlarConfig *kev_config);
-
-void kevlar_build_template(char *in_file_path, char *out_file_path, KevlarConfig *kev_config);
-
 void kevlar_parse_post_from_template(char * html_file_path, char * template_file_path, char * config_file_path, ListingItem * item);
+void kevlar_generate_listing(char * entry_template_file_path, char * config_file_path, ListingItem * itemsList, char * target, int size);
+void kevlar_generate_index_from_template( char * dist_path, char * template_folder_path, char * config_path, ListingItem * itemsList, int size);
 
 #endif
