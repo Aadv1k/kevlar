5428ee0be571e2df1b62d8bba7ba616ed9c39f39
Aadvik <aadvik.pandey@ezyr.io>
2025-10-10 11:14:50 +0530

    First successful seg fault :)

--- src/kevlar_markdown.h
+++ src/kevlar_markdown.h
@@ -1,5 +1,38 @@
 #ifndef _KEVLAR_MARKDOWN_H
 #define _KEVLAR_MARKDOWN_H
 
+#include <stdio.h>
+
+typedef enum e_NodeType {
+    MD_ROOT_NODE,
+
+    MD_TEXT_NODE,
+    MD_PAR_NODE,
+
+    MD_HEADING_NODE,
+} NodeType;
+
+typedef struct Md_Text_Opt {
+    char* data;
+    size_t len;
+} Md_Text_Opt;
+
+typedef struct Md_H_Opt {
+    unsigned char level;
+} Md_H_Opt;
+
+typedef struct Md_Ast {
+    NodeType node_type;
+    struct Md_Ast** children;
+    size_t c_count;
+    union {
+        Md_Text_Opt text_opt;
+        Md_H_Opt h_opt;
+    } opt;
+} Md_Ast;
+
+Md_Ast* kevlar_md_generate_ast(const char* source);
+
+void kevlar_md_free_ast(Md_Ast* ast);
 
 #endif //  _KEVLAR_MARKDOWN_H
--- tests/test_markdown.c
+++ tests/test_markdown.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
-
 #include "../src/kevlar_markdown.h"
 
-extern void test_markdown() {
-    printf("INFO: Running test_markdown()\n");
+#include <stdio.h>
+#include <assert.h>
+
+void test_md_heading() {
+    Md_Ast* ast;
+
+    ast = kevlar_md_generate_ast("# Foo");
+
+    assert(ast->c_count == 1 && "Single heading child");
+    assert(ast->children[ast->c_count]->node_type == MD_HEADING_NODE && "Child must be heading node ");
+
+    kevlar_md_free_ast(ast);
+}
+
+void test_md_basic() {
+    Md_Ast* ast = kevlar_md_generate_ast("Hello, World!");
+
+    assert(ast->node_type == MD_ROOT_NODE && "AST must have a root node");
+
+    kevlar_md_free_ast(ast);
+}
+
+void test_markdown() {
+    test_md_basic();
+    test_md_heading();
+
+    printf("SUCCESS: test_markdown()\n");
 };
